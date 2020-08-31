4b2c84e889789001ac717e12a6be67549e41f9d2
Raph Gallardo <draphick@gmail.com>
2020-08-31 15:56:15 -0700

    0009 Complete

--- 0009.specialPythageoreanTriplet.py
+++ 0009.specialPythageoreanTriplet.py
@@ -5,37 +5,30 @@ def thirteenth():
     """
         A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-        a2 + b2 = c2
-        For example, 32 + 42 = 9 + 16 = 25 = 52.
+        a**2 + b**2 = c**2
+        For example, 3**2 + 4**2 = 9 + 16 = 25 = 52.
 
         There exists exactly one Pythagorean triplet for which a + b + c = 1000.
         Find the product abc.
+
         https://projecteuler.net/problem=9
-
     """
-    topnum = 333
-    def isperfsquare(num):
-        return sqrt(num)
-
-
-    perfsquares = [sqrt(square)for square in range(1,topnum + 1)[::-1]]
-    # perfsquares = [sqrt(square)for square in range(1,topnum + 1)[::-1] if isperfsquare(square)]
-    print(perfsquares)
-    # for a in perfsquares:
-    #     for b in perfsquares:
-    #         if a != b:
-    #             if a < b:
-    #                 c = a + b
-    #                 if c in perfsquares:
-    #                     # print(a,b,c)
-    #                     a = int(sqrt(a))
-    #                     b = int(sqrt(b))
-    #                     c = int(sqrt(c))
-    #                     # print(a, 'squared plus', b, 'squared =', c, 'squared')
-    #                     # print('-----')
-    #                     # if sum([a,b,c]) == 1000:
-    #                     #     print('this')
-    #                     break
+    for a in range(1,1000):
+        for b in range(1,1000):
+            if a != b and a < b:
+                total = a ** 2 + b ** 2 
+                c = sqrt(total)
+                newtotal = sum([a,b,c])
+                if c == int(c):
+                    newtotal = sum([a,b,int(c)])
+                    if newtotal == 1000:
+                        answer = (a*b*int(c))
+                        print(a,b,int(c))
+                        print(a**2,b**2,int(c)**2)
+                        print(answer)
+                        break
+            else:
+                pass
 thirteenth()
 
  
